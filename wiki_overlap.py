62d2379cef9d2433ad12ee83df307a068e847a84
Sylvain Boissel <sylvain@ashtree.eu>
2015-05-31 23:27:00 +0200

    Overlap between the main Wikipedias

--- wiki_overlap.py
+++ wiki_overlap.py
@@ -29,7 +29,7 @@ def nb_items(url):
 
 print "Number of articles for each language"
 for i in lang_codes:
-    url = "https://wdq.wmflabs.org/api?q=link[" + i + "wiki]%20and%20noclaim[31:4167836]%20and%20noclaim[31:15647814]%20and%20noclaim[31:4167410]&noitems=1"
+    url = "https://wdq.wmflabs.org/api?q=link[" + i + "wiki]%20and%20noclaim[31:4167836]%20and%20noclaim[31:15647814]%20and%20noclaim[31:11266439]&noitems=1"
     lang_sizes[i] = float(nb_items(url))
     print i, nb_items(url) 
 
@@ -39,6 +39,6 @@ print "Percentage of common articles"
 for i in lang_codes:
     for j in lang_codes:
         if i < j:
-            url = "https://wdq.wmflabs.org/api?q=link[" + i  + "wiki]%20and%20link[" + j + "wiki]%20and%20noclaim[31:4167836]%20and%20noclaim[31:15647814]%20and%20noclaim[31:4167410]&noitems=1"
+            url = "https://wdq.wmflabs.org/api?q=link[" + i  + "wiki]%20and%20link[" + j + "wiki]%20and%20noclaim[31:4167836]%20and%20noclaim[31:15647814]%20and%20noclaim[31:11266439]&noitems=1"
             nb = nb_items(url)
             print "{} - {} : {} common items ({} % of {} Wikipedia - {} % of {} Wikipedia)".format(i, j, nb, round(nb / lang_sizes[i] * 100, 2), langs[i], round(nb / lang_sizes[j] * 100,2), langs[j])
